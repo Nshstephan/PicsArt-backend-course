09e8282ca4c9b2ecee1f9b086524c812fa8f87cb
Nshstephan <nshstephan@gmail.com>
2020-11-03 20:19:37 +0400

    Add files via upload

---
+++ primitiveTypes.java
@@ -0,0 +1,21 @@
+public class primitiveTypes {
+    public static void main(String[] args) {
+
+        int num1 = 30000;
+        byte num2 = 30;
+        short num3 = 3000;
+        long num4 = 300000;
+        char character = 'S';
+        boolean flag = true;
+        float dec1 = 0.1F;
+        double dec2 = 0.555;
+        System.out.println("integer is: " + num1);
+        System.out.println("byte is: " + num2);
+        System.out.println("short is: " + num3);
+        System.out.println("long is: " + num4);
+        System.out.println("char is: " + character);
+        System.out.println("boolean is: " + flag);
+        System.out.println("float is: " + dec1);
+        System.out.println("double is: " + dec2);
+    }
+}
