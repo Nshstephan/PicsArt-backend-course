14dbf075b69b16be5e79294457c53c1008bac031
Nshstephan <nshstephan@gmail.com>
2020-12-17 13:52:39 +0400

    isMatched

---
+++ isMatched.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+
+public class isMatched {
+
+
+    public static boolean isMatched(String expression) {
+        final String opening = "({[";
+        final String closing = ")}]";
+        Deque<Character> buffer = new LinkedList<>();
+        for (char c : expression.toCharArray()) {
+            if (opening.indexOf(c) != -1)
+                buffer.push(c);
+            else if (closing.indexOf(c) != -1) {
+                if (buffer.isEmpty())
+                    return false;
+                if (closing.indexOf(c) != opening.indexOf(buffer.pop()))
+                    return false;
+
+            }
+        }
+        return buffer.isEmpty();
+    }
+
+
+    public static void main(String[] args) {
+        String expr = "asdawd(DWQD[{wdqfer}ewfrwe])";
+
+        if (isMatched(expr))
+            System.out.println("Balanced ");
+        else
+            System.out.println("Not Balanced ");
+    }
+}
+
